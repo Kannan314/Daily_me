778671479fa62f49fc7f713ad3f31d250800c7e7
Kannan <Kannan@KANNAN>
2024-11-21 23:02:13 +0530

    21.11.24

--- Kannan/src/Project/Bracket_Match.java
+++ Kannan/src/Project/Bracket_Match.java
@@ -5,16 +5,15 @@ public class Bracket_Match {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		String a = sc.nextLine();
-        String openBrackets = "{([<";
-        String closeBrackets = "})]>";
+        String open = "{([<";
+        String close = "})]>";
         String temp = "";
-
         for (char c : a.toCharArray()) {
-            if (openBrackets.indexOf(c) != -1) {
+            if (open.indexOf(c) != -1) {
                 temp += c;
             } else {
-                int index = closeBrackets.indexOf(c);
-                if (temp.isEmpty() || openBrackets.indexOf(temp.charAt(temp.length() - 1)) != index) {
+                int index = close.indexOf(c);
+                if (temp.isEmpty() || open.indexOf(temp.charAt(temp.length() - 1)) != index) {
                     break;
                 }
                 temp = temp.substring(0, temp.length() - 1);
