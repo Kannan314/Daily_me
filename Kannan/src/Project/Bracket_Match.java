471d652cccb1076a82484353c1aa560f29f1358f
Kannan <Kannan@KANNAN>
2024-11-21 22:59:42 +0530

    21.11.24

--- Kannan/src/Project/Bracket_Match.java
+++ Kannan/src/Project/Bracket_Match.java
@@ -4,26 +4,25 @@ public class Bracket_Match {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		String input = sc.nextLine();
-        System.out.println(isBalanced(input) ? "YES" : "NO");
-    }
-
-    public static boolean isBalanced(String str) {
+		String a = sc.nextLine();
         String openBrackets = "{([<";
         String closeBrackets = "})]>";
         String temp = "";
 
-        for (char c : str.toCharArray()) {
+        for (char c : a.toCharArray()) {
             if (openBrackets.indexOf(c) != -1) {
                 temp += c;
             } else {
                 int index = closeBrackets.indexOf(c);
                 if (temp.isEmpty() || openBrackets.indexOf(temp.charAt(temp.length() - 1)) != index) {
-                    return false;
+                    break;
                 }
                 temp = temp.substring(0, temp.length() - 1);
             }
         }
-        return temp.isEmpty();
+        if(temp.isEmpty())
+        	System.out.println("YES");
+        else
+        	System.out.println("NO");
     }
 }
